cc73bde1819a08743929e87bafc6c03b8adbf207
João Matos <aluno21887@ipt.pt>
2021-03-17 12:37:57 +0000

    falta mt coisa

--- calculadora/calculadora/Controllers/HomeController.cs
+++ calculadora/calculadora/Controllers/HomeController.cs
@@ -1,13 +1,9 @@
 ﻿using calculadora.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace calculadora.Controllers
+namespace Calculadora.Controllers
 {
     public class HomeController : Controller
     {
@@ -18,13 +14,62 @@ namespace calculadora.Controllers
             _logger = logger;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
+            //prepara o valor inicial do visor
+            ViewBag.Visor = "0";
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public IActionResult Index(string botao, string visor)
         {
+
+            switch (botao)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                    //atribui ao 'visor' o valor do 'botão'
+                    if (visor == "0") visor = botao;
+                    else visor = visor + botao;
+                    break;
+
+                case "+/-":
+                    //faz a inversão do valor do visor
+                    if (visor.StartsWith('-')) visor = visor.Substring(1);
+                    else visor = '-' + visor;
+                    break;
+
+                case ",":
+                    if (!visor.Contains(',')) visor += ",";
+                    break;
+
+                case "+":
+                case "-":
+                case "x":
+                case ":":
+                    string primeiroOperando = '';
+                    string Operador = "";
+                    string limpaVisor ="Sim"
+
+
+
+                    break;
+            }//fim do switch
+
+            //enviar o valor do visor para a view
+            ViewBag.Visor = visor;
+            ViewBag.PrimeiroOperador = "Nao";
+
             return View();
         }
 
